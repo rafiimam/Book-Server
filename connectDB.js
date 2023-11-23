5b8902be3ca13fdf2344e9a10d6930b01840b8e0
Rafi Imam <rafiimam96@gmail.com>
2023-11-23 23:16:05 +0600

    new update

--- connectDB.js
+++ connectDB.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Remove useFindAndModify and useCreateIndex options
+    });
+
     console.log(`Database Connected ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
 };
